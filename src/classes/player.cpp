35e8f87d79591b6d9a3ea8ebf711e54addd23432
Cosmonium505 <jackrrpearson@gmail.com>
2025-05-16 20:25:43 -0400

    Added worldpos.

--- src/classes/player.cpp
+++ src/classes/player.cpp
@@ -4,7 +4,7 @@
 
 void Player::render(SDL_Renderer* renderer) {
     SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], 255);
-    SDL_Rect rect = { static_cast<int>(pos[0]), static_cast<int>(pos[1]), static_cast<int>(size[0]), static_cast<int>(size[1]) };
+    SDL_Rect rect = { static_cast<int>(pos[0] + engineParams.currentWorld->worldpos[0]), static_cast<int>(pos[1] + engineParams.currentWorld->worldpos[1]), static_cast<int>(size[0]), static_cast<int>(size[1])};
     SDL_RenderFillRect(renderer, &rect);
 }
 
